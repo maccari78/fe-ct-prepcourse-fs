dd9c3d429f35cfe2408c8efe239f4c2ee804e102
Danilo Maccari <maccari78@gmail.com>
2023-11-27 19:59:51 -0300

    correccion

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -29,8 +29,6 @@ function combinarNombres(nombre, apellido) {
   // Retorna "nombre" y "apellido" combinados en un mismo string pero separados por un espacio.
   // Ejemplo: ("Soy", "Henry") ---> "Soy Henry"
   // Tu código:
-  var nombre = "Soy";
-  var apellido = "Henry";
   return nombre + " " + apellido;
 }
 
